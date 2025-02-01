09bb8b3a9fead21963289319328d4661e2a9512d
pkkronborg <59437536+pkkronborg@users.noreply.github.com>
2025-02-01 23:34:39 +0100

    footer styling

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
-    <footer className="p-3 bg-light mt-3">
-      <div className="d-flex justify-content-center">Copyright</div>
+    <footer className="p-3 bg-primary mt-3">
+      <div className="d-flex justify-content-center text-white">
+        &copy; Holidaze
+      </div>
     </footer>
   );
 }
